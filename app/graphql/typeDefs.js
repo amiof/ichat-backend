a1b3042c8bc52612ce0fe70db411cbc04925895d
amiof <arm131313@gmail.com>
2023-02-22 09:17:41 +0330

    add types of models to graphql

--- app/graphql/typeDefs.js
+++ app/graphql/typeDefs.js
@@ -3,9 +3,42 @@ import { gql } from "apollo-server-express";
 const typeDefs = gql`
   type Query {
     data: String
+    nameSpaces: [nameSpace]
+    rooms: [room]
+    users: [user]
   }
+  type nameSpace {
+    id: ID
+    name: String
+    pic: String
+    title: String
+    rooms: [ID]
+  }
+  type room {
+    id: ID
+    name: String
+    pic: String
+    title: String
+    endPoint: String
+    description: String
+    messages: [message]
+  }
+  type message {
+    id: ID
+    sender: ID
+    message: String
+  }
+  type user {
+    id: ID
+    username: String
+    password: String
+    rooms: [ID]
+    nameSpace: [ID]
+    phoneNumber: String
+  }
+
   type Mutation {
-    data: String
+    data(server: String): String
   }
 `;
 export default typeDefs;
